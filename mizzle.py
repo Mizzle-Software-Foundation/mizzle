8bbed479bd715ad55b069c8cee745410571feff6
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-07 14:12:56 -0700

    update

--- mizzle.py
+++ mizzle.py
@@ -2,5 +2,5 @@ import subprocess
 import os
 
 compiledir = os.path.dirname(os.path.abspath(__file__))
-script = os.path.abspath(os.path.join(this_dir, "src", "compile.py"))
+script = os.path.abspath(os.path.join(this_dir, "src", "parse.py"))
 subprocess.run([sys.executable, script], check=True)
