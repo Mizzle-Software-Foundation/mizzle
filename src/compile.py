681282197d08082e71f0f0d1ff332b14d024eb60
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-07 13:58:57 -0700

    hrrmmm

--- src/compile.py
+++ src/compile.py
@@ -1,3 +1,5 @@
 import parse
-file = os.path.abspath(os.path.join(this_dir, "parse.py"))
+file = os.path.abspath(os.path.join(this_dir, .., "mizzle.py"))
 parse.parsed_commands()
+import file
+compileto = file.compiledir()
