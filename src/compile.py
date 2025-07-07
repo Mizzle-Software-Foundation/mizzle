ac9a51ac117fa975f6742278214fba64e603c0c5
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-07 14:38:44 -0700

    hehe

--- src/compile.py
+++ src/compile.py
@@ -1,16 +1,7 @@
+import subprocess
 import os
-import parse
-import importlib.util
-thisdir = os.path.dirname(os.path.abspath(__file__)) # the dir we run from idiot!
-mizzle_py_path = os.path.abspath(os.path.join(thisdir, "..", "mizzle.py"))
-spec = importlib.util.spec_from_file_location("mizzle", mizzle_py_path)
-mizzle = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mizzle)
+import sys
 
-# yeah, yeah let me do it.
-commands_to_compile = parse.parsed_commands()
-file = parse.mz_file()
-
-with open(file, "w") as f:
-    for line in commands_to_compile:
-        f.write(line + "\n")
+thisdir = os.path.dirname(os.path.abspath(__file__))  # the dir we run from idiot!
+script = os.path.abspath(os.path.join(thisdir, "src", "parse.py"))
+subprocess.run([sys.executable, script], check=True)
