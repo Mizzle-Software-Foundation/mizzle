419191ffbec7f8c558470f1eec860ba5bac02034
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-07 14:10:20 -0700

    e

--- src/compile.py
+++ src/compile.py
@@ -1,7 +1,6 @@
 import os
 import parse
 import importlib.util
-parsed_commands_list = parse.parsed_commands()
 thisdir = os.path.dirname(os.path.abspath(__file__)) # the dir we run from idiot!
 mizzle_py_path = os.path.abspath(os.path.join(thisdir, "..", "mizzle.py"))
 spec = importlib.util.spec_from_file_location("mizzle", mizzle_py_path)
@@ -9,5 +8,12 @@ mizzle = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(mizzle)
 
 # yeah, yeah let me do it.
-mizzle.parse.parsed_commands()
+commands_to_compile = parse.parsed_commands()
 compileto = mizzle.compiledir()
+filename = None # imma add this bro, i need to add the parser and stuff tho.
+
+fileoutput = f"{compileto}{filename}"
+
+with open(fileoutput, "w") as f:
+    for line in commands_to_compile:
+        f.write(line + "\n")
