10dc3e7df353283e8d77c8e2016129624b9f593c
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-07 14:32:49 -0700

    hehe

--- src/parse.py
+++ src/parse.py
@@ -1,6 +1,16 @@
+import os
 # there is gonna be a lot of parsing, becuase of the massicve amount of syntax to be added.
 commands = []
 current_command = []
 parsed_commands = []
 parsed_command = []
 mz_file = input("Enter the path to your .mz file: ")
+if os.path.exists(mz_file):
+  pass
+else:
+  while True:
+    mz_file = input("Enter the path to your .mz file: ")
+    if os.path.exists(mz_file):
+      break
+    else:
+      continue
