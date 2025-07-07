47425d5597f1e058ef0078f2028db7eebb7bdd27
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-07 14:05:25 -0700

    compiler thing

--- src/compile.py
+++ src/compile.py
@@ -1,5 +1,13 @@
+import os
 import parse
-file = os.path.abspath(os.path.join(this_dir, .., "mizzle.py"))
-parse.parsed_commands()
-import file
-compileto = file.compiledir()
+import importlib.util
+parsed_commands_list = parse.parsed_commands()
+thisdir = os.path.dirname(os.path.abspath(__file__)) # the dir we run from idiot!
+mizzle_py_path = os.path.abspath(os.path.join(thisdir, "..", "mizzle.py"))
+spec = importlib.util.spec_from_file_location("mizzle", mizzle_py_path)
+mizzle = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(mizzle)
+
+# yeah, yeah let me do it.
+mizzle.parse.parsed_commands()
+compileto = mizzle.compiledir()
