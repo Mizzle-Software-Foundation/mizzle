58e05b216f2a5f586d8817266c4191c153690f65
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-07 13:58:06 -0700

    final

--- mizzle.py
+++ mizzle.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
 
+compiledir = os.path.dirname(os.path.abspath(__file__))
 script = os.path.abspath(os.path.join(this_dir, "src", "compile.py"))
 subprocess.run([sys.executable, script], check=True)
