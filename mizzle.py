fcdd4f5431308ac9229634daa8cdcbe3bec04e12
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-27 19:56:33 -0700

    that got deleted! oops!

--- mizzle.py
+++ mizzle.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
 
+thisdir = os.path.dirname(os.path.abspath(__file__))  # the dir we run from idiot!
 script = os.path.abspath(os.path.join(this_dir, "src", "parse.py"))
 subprocess.run([sys.executable, script], check=True)
